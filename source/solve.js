1aad8339a9ea877ae94ea46de9b51a3fc1b5e595
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-02-12 01:43:35 +0300

    added JSDoc, test names. changed some checks

--- source/solve.js
+++ source/solve.js
@@ -1,28 +1,44 @@
-function CustomError( message ) {
-	this.message = message;
-}
+'use strict';
+
+/**
+ * the function save the custom exception
+ * @param {string} message - the description of the error
+ */
+
+function CustomError(message) { this.message = message; }
+
+/**
+ * the function get the description of the error
+ * @returns {string} this.message - the description of the error
+ */
 
 CustomError.prototype.toString = function() { return this.message; };
+
+/**
+ * the function solves the math expression with x variable
+ * @param {string} expr - the math expression
+ * @param {integer} _x - the value of the x variable in expression
+ * @returns {integer} - the calculated value of math expression
+ */
 
 const solve = (expr, _x) => {
 	if (typeof(expr) !== "string") {
 		throw new CustomError("The expression should be a string");
 	}
-
-	if (isNaN(_x)) {
+	let x = +_x;
+	if (Number.isNaN(x)) {
 		throw new CustomError("The value should be initialize");
 	}
 
-	let x = +_x;
 	expr = expr.toLowerCase();
 
-	if (!expr.match(/^[x\d/\*\+\-( )]*$/) 
+	if (!expr.match(/^[x\d/\*\+\-( )]*$/)
 		|| expr.match(/(xx+)|(\*\*+)|(\-\-+)|(\+\++)/)) {
 		throw new CustomError("Incorrect symbols in expression")
 	}
 	let result = Function('x', 'return (' + expr + ');')(x);
-	if (isNaN(result)) { 
-		throw CustomError("Expression is incorrect");
+	if (Number.isNaN(result)) {
+		throw new CustomError("Expression is incorrect");
 	}
-	return result; 
+	return result;
 }
