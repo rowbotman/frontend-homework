0501b9724a473441be95bec48e4a0014f382c54a
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-03-11 14:54:40 +0300

    added returns object with error msg and result value

--- source/solve.js
+++ source/solve.js
@@ -1,9 +1,35 @@
 'use strict';
 
 /**
+ * Custom class of errors and results
+ */
+class SolveResult {
+	constructor(value, msg = NaN) {
+		this.value = value;
+		this.error = msg;
+	}
+
+	/**
+	 * Get result solve function
+	 * @returns {number} - result value of solve function
+	 */
+	getResult() {
+		return this.value;
+	}
+
+	/**
+	 * Get error message. If solve() have no errors, this return NaN
+	 * @returns {string} - error message
+	 */
+	errors() {
+		return this.error;
+	}
+}
+
+/**
  * the function get type of object
- * @param obj
- * @returns {string}
+ * @param obj - object
+ * @returns {string} - result type
  */
 const toType = obj =>
 		({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
@@ -12,13 +38,17 @@ const toType = obj =>
  * the function solves the math expression with x variable
  * @param {string} expr - the math expression
  * @param {integer} _x - the value of the x variable in expression
- * @returns {number} - the calculated value of math expression
+ * @returns {SolveResult} - the calculated value of math expression with error message (if no errors it is NaN)
  */
 const solve = (expr, _x) => {
-	if (toType(expr) !== 'string') { return NaN; }
+	if (toType(expr) !== 'string') {
+		return new SolveResult(NaN, "the expression should have a string type")
+	}
 
 	const x = +_x;
-	if (Number.isNaN(x)) { return NaN; }
+	if (Number.isNaN(x)) {
+		return new SolveResult(NaN, "the second argument should be a number");
+	}
 
 	const lowerExpr = expr.toLowerCase();
 	const symPattern = new RegExp(/^[x\d/\*\+\-()' '\t]*$/);
@@ -27,12 +57,15 @@ const solve = (expr, _x) => {
 		&& !syntaxPattern.test(lowerExpr)) {
 		try {
 			let result = Function('x', `return (${lowerExpr});`)(x);
-			if (Number.isNaN(result)) { return NaN; }
+			if (Number.isNaN(result)) {
+				return new SolveResult(NaN, "calculate errors");
+			}
 
-			return result;
+			return new SolveResult(result);
 		} catch (e) {
-			return NaN;
+			console.log(e);
+			return new SolveResult(NaN, "syntax error");
 		}
 	}
-	return NaN;
+	return new SolveResult(NaN, "incorrect symbols in expression")
 };
--- tests/solve.js
+++ tests/solve.js
@@ -1,31 +1,32 @@
 'use strict';
 
-QUnit.module('Тестируем функцию solve', function () {
-	QUnit.test('solve работает правильно ', function (assert) {
-		assert.strictEqual(solve('x + 1', 1), 2);
-		assert.strictEqual(solve('2 + x - 1', 5), 6);
-		assert.strictEqual(solve('2 * x - 1', 5), 9);
-		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
-		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
-		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
-	});
+QUnit.module('Тестируем функцию solve', function (number) {
+  QUnit.test('solve работает правильно ', function (assert) {
+    assert.strictEqual(solve('x + 1', 1).getResult(), 2);
+    assert.strictEqual(solve('2 + x - 1', 5).getResult(), 6);
+    assert.strictEqual(solve('2 * x - 1', 5).getResult(), 9);
+    assert.strictEqual(solve('2 * ( x - 1 )', 5).getResult(), 8);
+    assert.strictEqual(solve('(5 - x) * (x + 5)', 3).getResult(), 16);
+    assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3).getResult(), 144);
+  });
 
-	QUnit.test('solve корректно обрабатывает ошибки ', function (assert) {
-		assert.strictEqual(
-			isNaN(solve(11, 11)),
-			true,
-			"Вместо строки-выражения передали число");
-		assert.strictEqual(
-			isNaN(solve("x * x")),
-			true,
-			"Не передан x");
-		assert.strictEqual(
-			isNaN(solve("xx * 10 -1", 10)),
-		 	true,
-		 	"Лишние символы в выражении");
-		assert.strictEqual(
-			isNaN(solve("(x * x", 10)),
-			true,
-			"Ошибки при использовании операторов");
-	});
+  QUnit.test('solve корректно обрабатывает ошибки ', function (assert) {
+    assert.strictEqual(
+        isNaN(solve(11, 11).error),
+        true,
+        'Вместо строки-выражения передали число');
+    assert.strictEqual(
+        isNaN(solve('x * x').error),
+        true,
+        'Не передан x');
+    assert.strictEqual(
+        isNaN(solve('xx * 10 -1', 10).error),
+        true,
+        'Лишние символы в выражении');
+    assert.strictEqual(
+        isNaN(solve('(x * x - 10', 12).error),
+        true,
+        'Ошибки при использовании операторов'
+        );
+  });
 });
